4ece1787d9ce031326734795bc780fdc5dbc3559
leila-digh <leiladabakuyo@cmail.carleton.ca>
2025-10-24 15:34:24 -0400

    commit

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+const [firstAnimal, second, third] = ["giraffe", "zebra", "bear"];
+console.log(firstAnimal);
+console.log(second);
+console.log(third);
+
+
+
 createRoot(document.getElementById("root")).render(<App />);
